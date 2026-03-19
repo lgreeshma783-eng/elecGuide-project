06b83778e5500567dd6a9dfee7f3118841eacf85
Greeshma <lgreeshma783@gmail.com>
2026-03-19 17:03:09 +0530

    sample

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
+
 import { useTitle } from "../hooks/useTitle";
 import { Rating } from "../components";
 import { useCart } from "../context";
