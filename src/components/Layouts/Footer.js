839cca5599b68fbcb01d51116951c03137567ca8
Greeshma <lgreeshma783@gmail.com>
2026-03-19 17:12:28 +0530

    Updated footer

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-    <footer className="bg-white shadow dark:bg-gray-800">
+    <footer className="bg-white shadow dark:bg-gray-900">
         <div className="p-4 mx-auto max-w-screen-xl md:flex md:items-center md:justify-between md:p-6">            
             <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">CodeBook</Link>. All Rights Reserved.</span>
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
