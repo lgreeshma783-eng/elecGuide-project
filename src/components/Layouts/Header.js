14dbe7fefe1fac7bda1a5696b41a632c614d473b
Greeshma <lgreeshma783@gmail.com>
2026-06-03 20:55:19 +0530

    updated

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -27,7 +27,7 @@ export const Header = () => {
       <nav className="bg-white dark:bg-gray-900">
           <div className="border-b border-slate-200 dark:border-b-0 flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-3">
               <Link to="/" className="flex items-center">
-                  <img src={Logo} className="mr-3 h-10" alt="CodeBook Logo" />
+                  <img src={Logo} className="mr-3 h-10" alt="ElecGuide Logo" />
                   <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ElecGuide</span>
               </Link>
               <div className="flex items-center relative">
