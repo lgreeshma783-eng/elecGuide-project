3dfb5c27c56790a8ea89112014c8a06992864866
Greeshma <lgreeshma783@gmail.com>
2026-06-03 20:40:47 +0530

    updated to rupees

--- src/pages/Dashboard/components/DashboardCard.js
+++ src/pages/Dashboard/components/DashboardCard.js
@@ -5,7 +5,7 @@ export const DashboardCard = ({order}) => {
     <div className="max-w-4xl m-auto p-2 mb-5 border dark:border-slate-700">
         <div className="flex justify-between text-sm m-2 font-bold dark:text-slate-200">
             <span>Order Id: {order.id}</span>
-            <span>Total: ${order.amount_paid}</span>
+            <span>Total: &#8377;{order.amount_paid}</span>
         </div>
         { order.cartList.map((product) => (
             <div key={product.id} className="flex flex-wrap justify-between max-w-4xl m-auto p-2 my-5 ">
@@ -18,7 +18,7 @@ export const DashboardCard = ({order}) => {
                             <p className="text-lg ml-2 dark:text-slate-200">{product.name}</p>
                         </Link>
                         <div className="text-lg m-2 dark:text-slate-200">
-                            <span>${product.price}</span>
+                            <span>&#8377;{product.price}</span>
                         </div>
                     </div>
                 </div>
